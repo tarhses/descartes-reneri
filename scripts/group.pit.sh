5c73f945bd34ffc2485edd142f4b07066789736e
Pierre Luycx <pierre.luycx@gmail.com>
2022-03-19 14:12:51 +0100

    Add report.sh wapping to group scripts

--- scripts/group.pit.sh
+++ scripts/group.pit.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-# This script must be used in a base directory that contains pom.pit.xml and the SUT
+# This script must be used in a base directory that contains pom.pit.xml,
+# report.pit.sh, and the SUT
 
 # Swap the SUT's pom.xml
 cp pom.pit.xml 2048/pom.xml
+cp report.pit.sh 2048/report.sh
--- scripts/group.reneri.sh
+++ scripts/group.reneri.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-# This script must be used in a base directory that contains pom.reneri.xml and the SUT
+# This script must be used in a base directory that contains pom.reneri.xml,
+# report.reneri.sh, and the SUT
 
 # Swap the SUT's pom.xml
 cp pom.reneri.xml 2048/pom.xml
+cp report.reneri.sh 2048/report.sh
